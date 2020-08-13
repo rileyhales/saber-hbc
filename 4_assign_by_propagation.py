f03c54cbde053030e9203675bfb9d90dea87d248
rileyhales <rileyhales1@gmail.com>
2020-08-13 15:48:36 -0600

    some updates to clustering assignments

--- 4_assign_by_propagation.py
+++ 4_assign_by_propagation.py
@@ -2,105 +2,145 @@ import geopandas as gpd
 import pandas as pd
 
 
-def find_downstream_ids(df: pd.DataFrame, target_id: int, same_order: bool = True):
-    downstream_ids = [target_id, ]
-    stream_row = df[df['COMID'] == target_id]
-    stream_order = stream_row['order_'].values[0]
+def walk_downstream(df: pd.DataFrame, target_id: int, id_col: str, next_col: str, order_col: str = None,
+                    same_order: bool = True, outlet_next_id: str or int = -1) -> tuple:
+    """
+    Traverse a stream network table containing a column of unique ids, a column of the id for the stream/basin
+    downstream of that point, and, optionally, a column containing the stream order.
 
+    Args:
+        df (pd.DataFrame):
+        target_id (int):
+        id_col (str):
+        next_col (str):
+        order_col (str):
+        same_order (bool):
+        outlet_next_id (str or int):
+
+    Returns:
+        Tuple of stream ids in the order they come from the starting point.
+    """
+    downstream_ids = [target_id, ]
+
+    df_ = df.copy()
     if same_order:
-        while stream_row['NextDownID'].values[0] != -1 and stream_row['order_'].values[0] == stream_order:
-            downstream_ids.append(stream_row['NextDownID'].values[0])
-            stream_row = df[df['COMID'] == stream_row['NextDownID'].values[0]]
-            if len(stream_row) == 0:
-                break
-    else:
-        while stream_row['NextDownID'].values[0] != -1:
-            downstream_ids.append(stream_row['NextDownID'].values[0])
-            stream_row = df[df['COMID'] == stream_row['NextDownID'].values[0]]
-            if len(stream_row) == 0:
-                break
+        df_ = df_[df_[order_col] == df_[df_[id_col] == target_id][order_col].values[0]]
+
+    stream_row = df_[df_[id_col] == target_id]
+    while stream_row[next_col].values[0] != outlet_next_id:
+        downstream_ids.append(stream_row[next_col].values[0])
+        stream_row = df_[df_[id_col] == stream_row[next_col].values[0]]
+        if len(stream_row) == 0:
+            break
     return tuple(downstream_ids)
 
 
-def find_upstream_ids(df: pd.DataFrame, target_id: int, same_order: bool = True):
-    d = df.copy()
-    if same_order:
-        d = d[d['order_'] == d[d['COMID'] == target_id]['order_'].values[0]]
+def walk_upstream(df: pd.DataFrame, target_id: int, id_col: str, next_col: str, order_col: str = None,
+                  same_order: bool = True) -> tuple:
+    """
+    Traverse a stream network table containing a column of unique ids, a column of the id for the stream/basin
+    downstream of that point, and, optionally, a column containing the stream order.
 
+    Args:
+        df (pd.DataFrame):
+        target_id (int):
+        id_col (str):
+        next_col (str):
+        order_col (str):
+        same_order (bool):
+
+    Returns:
+        Tuple of stream ids in the order they come from the starting point. If you chose same_order = False, the
+        streams will appear in order on each upstream branch but the various branches will appear mixed in the tuple in
+        the order they were encountered by the iterations.
+    """
+    df_ = df.copy()
+    if same_order:
+        df_ = df_[df_[order_col] == df_[df_[id_col] == target_id][order_col].values[0]]
+
+    # start a list of the upstream ids
     upstream_ids = [target_id, ]
-    upstream_rows = d[d['NextDownID'] == target_id]
+    upstream_rows = df_[df_[next_col] == target_id]
 
     while not upstream_rows.empty or len(upstream_rows) > 0:
         if len(upstream_rows) == 1:
-            upstream_ids.append(upstream_rows['COMID'].values[0])
-            upstream_rows = d[d['NextDownID'] == upstream_rows['COMID'].values[0]]
+            upstream_ids.append(upstream_rows[id_col].values[0])
+            upstream_rows = df_[df_[next_col] == upstream_rows[id_col].values[0]]
         elif len(upstream_rows) > 1:
-            for id in upstream_rows['COMID'].values.tolist():
-                upstream_ids += list(find_upstream_ids(d, id, False))
-                upstream_rows = d[d['NextDownID'] == id]
+            for id in upstream_rows[id_col].values.tolist():
+                upstream_ids += list(walk_upstream(df_, id, False))
+                upstream_rows = df_[df_[next_col] == id]
     return tuple(set(upstream_ids))
 
 
 def clip_drainage_lines_by_ids(list_of_ids):
     a = gpd.read_file('/data_0_inputs/magdalena_drainagelines/south_americageoglowsdrainag.shp')
-    a[a['COMID'].isin(list_of_ids)].to_file('/Users/riley/code/basin_matching/data_4_assign_propagation/clipped_lines.json', driver='GeoJSON')
+    a[a['COMID'].isin(list_of_ids)].to_file(
+        '/Users/rileyhales/code/basin_matching/data_4_assign_propagation/clipped_lines.json', driver='GeoJSON')
     return
 
 
-def propagation_assignments(df: pd.DataFrame, station: int, ids: list or tuple,
-                            max_propagation: int = 5) -> pd.DataFrame:
-    df_cp = df.copy()
+def propagation_assignments(df: pd.DataFrame, station: int, ids: tuple, max_propagation: int = 5) -> pd.DataFrame:
     for distance, i in enumerate(ids):
         if distance + 1 >= max_propagation:
             print('distance is too large, no longer making assignments')
             continue
         try:
             # if the stream segment has an observation station in it, skip
-            if df_cp[df_cp['GeoglowsID'] == i]['AssignmentReason'].values[0] == 'spatial':
+            if df[df['GeoglowsID'] == i]['AssignmentReason'].values[0] == 'spatial':
                 print('found another station, skipping to next station')
                 continue
             # if the stream segment does not have an assigned value, assign it
-            if pd.isna(df_cp[df_cp['GeoglowsID'] == i]['AssignedID'].values[0]):
-                df_cp.loc[df_cp['GeoglowsID'] == i, 'AssignedID'] = station
-                df_cp.loc[df_cp['GeoglowsID'] == i, 'AssignmentReason'] = f'Propagation-{distance + 1}'
+            if pd.isna(df[df['GeoglowsID'] == i]['AssignedID'].values[0]):
+                df.loc[df['GeoglowsID'] == i, 'AssignedID'] = station
+                df.loc[df['GeoglowsID'] == i, 'AssignmentReason'] = f'Propagation-{distance + 1}'
                 print('assigned')
             # if the stream segment does have an assigned value, check if this one is better before overwriting
             else:
-                last_dist = int(df_cp[df_cp['GeoglowsID'] == i]['AssignmentReason'].values[0].split('-')[-1])
+                last_dist = int(df[df['GeoglowsID'] == i]['AssignmentReason'].values[0].split('-')[-1])
                 print(f'last distance: {last_dist}, new distance: {distance + 1}')
                 if distance + 1 < int(last_dist):
-                    df_cp.loc[df_cp['GeoglowsID'] == i, 'AssignedID'] = station
-                    df_cp.loc[df_cp['GeoglowsID'] == i, 'AssignmentReason'] = f'Propagation-{distance + 1}'
+                    df.loc[df['GeoglowsID'] == i, 'AssignedID'] = station
+                    df.loc[df['GeoglowsID'] == i, 'AssignmentReason'] = f'Propagation-{distance + 1}'
                     print('made better assignment')
                 continue
         except Exception as e:
             print(e)
             print(f'failed to set assigned value for geoglows id {i}')
-    return df_cp
+    return df
 
 
 def make_assignments_table():
-    sim_table = pd.read_csv('/Users/riley/code/basin_matching/data_0_inputs/magdalena_table.csv')
-    assignments_df = pd.DataFrame({'GeoglowsID': sim_table['COMID'].tolist(), 'Order': sim_table['order_'].tolist()})
-    obs_table = pd.read_csv('/Users/riley/code/basin_matching/data_0_inputs/magdalena_stations_assignments.csv')
+    sim_table = pd.read_csv('/Users/rileyhales/code/basin_matching/data_0_inputs/magdalena_table.csv')
+    assignments_df = pd.DataFrame({'GeoglowsID': sim_table['COMID'].tolist(), 'Order': sim_table['order_'].tolist(),
+                                   'Drainage': sim_table['Tot_Drain_'].tolist()})
+    obs_table = pd.read_csv('/Users/rileyhales/code/basin_matching/data_0_inputs/magdalena_stations_assignments.csv')
     assignments_df = pd.merge(assignments_df, obs_table, on='GeoglowsID', how='outer')
-    assignments_df.to_csv('/Users/riley/code/basin_matching/data_4_assignments/AssignmentsTable.csv', index=False)
+    assignments_df.to_csv('/Users/rileyhales/code/basin_matching/data_4_assignments/AssignmentsTable.csv', index=False)
     return
 
+make_assignments_table()
+exit()
 
-sim_table = pd.read_csv('/Users/riley/code/basin_matching/data_0_inputs/magdalena_table.csv')
-obs_table = pd.read_csv('/Users/riley/code/basin_matching/data_0_inputs/magdalena_stations_assignments.csv')
-assignments_df = pd.read_csv('/Users/riley/code/basin_matching/data_4_assignments/AssignmentsTable.csv')
 
-# for every station that we have
+sim_table = pd.read_csv('/Users/rileyhales/code/basin_matching/data_0_inputs/magdalena_table.csv')
+obs_table = pd.read_csv('/Users/rileyhales/code/basin_matching/data_0_inputs/magdalena_stations_assignments.csv')
+assignments_df = pd.read_csv('/Users/rileyhales/code/basin_matching/data_4_assignments/AssignmentsTable.csv')
+
+id_col = 'COMID'
+order_col = 'order_'
+next_col = 'NextDownID'
+
+# at each station
 for station in assignments_df['StationID'].dropna():
-    # determine the simulated id for the station
+    # determine the simulated id for the basin that contains the station
     geoglowsid = assignments_df[assignments_df['StationID'] == station]['GeoglowsID'].values[0]
-    # identify simulated ids downstream of the station's simulated id and assign them the station id
-    down_ids = find_downstream_ids(sim_table, geoglowsid, same_order=True)
+    # identify ids of simulated basins downstream of the station and assign them this station's id
+    down_ids = walk_downstream(sim_table, geoglowsid, id_col, order_col, next_col, same_order=True)
     assignments_df = propagation_assignments(assignments_df, station, down_ids, max_propagation=5)
-    # identify simulated ids upstream of the station's simulated id and assign them that station
-    up_ids = find_upstream_ids(sim_table, geoglowsid, same_order=True)
+    # identify ids of simulated basins upstream of the station and assign them this station's id
+    up_ids = walk_upstream(sim_table, geoglowsid, id_col, order_col, next_col, same_order=True)
     assignments_df = propagation_assignments(assignments_df, station, up_ids, max_propagation=5)
 
-assignments_df.to_csv('/Users/riley/code/basin_matching/data_4_assignments/AssignmentsTable_modify.csv', index=False)
+assignments_df.to_csv('/Users/rileyhales/code/basin_matching/data_4_assignments/AssignmentsTable_modify.csv',
+                      index=False)
--- 5_assign_by_clusters.py
+++ 5_assign_by_clusters.py
@@ -1,81 +1,102 @@
-import os
 import glob
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-import natsort
+
+
+def assign_gauges_to_spatial_clusters(cluster: pd.DataFrame, assigns: pd.DataFrame):
+    """
+    Scenario 1: SpatiallyClustered
+    There is a gauged stream, of the same order as the ungauged, spatially in your cluster. Preference to the station
+    with the closest upstream drainage areas when there are multiple options
+
+    options (pd.DataFrame): a subset of the assignments dataframe which lists only the simulated basins that also
+    contain observation data
+
+    Args:
+        cluster (gpd.GeoDataFrame): the geopandas geodataframe of the clustered basins
+        assigns (pd.Dataframe): the dataframe of the assigned and unassigned basins
+
+    """
+    # todo use the geojson of observed data points to limit the assignments df to only points within the cluster
+    # drop any of the basins that have already been assigned an ID to identify which still need assignment
+    needs_assignment = cluster.drop(cluster[cluster['COMID'].isin(assignments['AssignedID'].dropna())].index).values
+    # identify the stream orders in the basins needing to be assigned
+    stream_orders = sorted(set(assignments[assignments['GeoglowsID'].isin(cluster['COMID'])]['Order'].values))
+
+    # for each stream order with unassigned basins
+    for stream_order in stream_orders:
+        # filter a subset dataframe of assignments showing the simulated ID's that contain a station
+        opts_to_assign = assigns[assigns['AssignmentReason'] == 'spatial']
+        # the filter the remaining options to only include the stream order we're on
+        opts_to_assign = opts_to_assign[opts_to_assign['Order'] == stream_order]
+
+        # if there were no station points, we cannot apply this option
+        if opts_to_assign.empty:
+            print(f'For stream order {stream_order}, there were no gauges found and so none can be assigned')
+            continue
+
+        # if there is only 1 station option, apply that one to all ungauged basins of the same stream id
+        elif len(opts_to_assign) == 1:
+            for i in needs_assignment:
+                assigns.loc[assigns['GeoglowsID'] == i, 'AssignedID'] = opts_to_assign['COMID'].values[0]
+                assigns.loc[assigns['GeoglowsID'] == i, 'AssignmentReason'] = 'SpatiallyClustered'
+
+        # if there are multiple station options, assign the station with the upstream drainage area most similar to the
+        # each of the ungauged basins
+        else:
+            print(opts_to_assign)
+            print(cluster)
+            drain_areas =
+            drain_areas = cluster.merge(opts_to_assign, left_on='COMID', right_on='GeoglowsID', how='right')
+            drain_areas = drain_areas[['GeoglowsID', 'Tot_Drain_']]
+            print(drain_areas)
+            for i in needs_assignment:
+                print(i)
+                # find the drainage area of the simulated basin
+                da = cluster[cluster['COMID'] == i]['Tot_Drain_'].values[0]
+                # find the simulated basin which contains a station that has the nearest drainage area
+                closest = opts_to_assign['GeoglowsID'].values[(np.abs(drain_areas['Tot_Drain_'].values - da)).argmin()]
+                # then read what StationID is in the basin that matched by closest drainage area
+                station = opts_to_assign[opts_to_assign['GeoglowsID'] == closest]['StationID'].values[0]
+                # and then finally assign that station id to the ungauged simulated basin
+                print('assigned by spatial cluster and matched by area')
+                assigns.loc[assigns['GeoglowsID'] == i, 'AssignedID'] = station
+                assigns.loc[assigns['GeoglowsID'] == i, 'AssignmentReason'] = 'SpatiallyClusteredwArea'
+    return assigns
+
+
+def assign_intersecting_clusters():
+    # Apply option 2: when there is not a gauge of the correct order spatially within a cluster of simulated basins,
+    # cluster the gauges and see if there is a gauge of the right order in the cluster of one of the gauges that is
+    # spatially within the basin
+    return
+
+
+# todo: apply a scalar from the ratio of the ungauged and gauged upstream areas??
+# todo: perform a regression of drainage area v cumulative volume??
 
 # dl = gpd.read_file('data_0_inputs/magdalena_drainagelines/magdalena_drainagelines.shp')
 # ctch = gpd.read_file('data_0_inputs/magdalena_catchments/magdalena_catchments.shp')
-obs_pts = gpd.read_file('data_0_inputs/ideam_stations.json').to_crs(epsg=3857)
-assigns = pd.read_csv('data_4_assignments/geoglowsID_stationID_assignedID.csv')
 
-for cluster in glob.glob('/Users/riley/code/basin_matching/data_3_pairbasins/geojson-sim-6/*.geojson'):
+# identify any of the observation points that are in this cluster (returns a df of points) (should always have 1+)
+# obs_pts = gpd.read_file('data_0_inputs/ideam_stations.json').to_crs(epsg=3857)
+# obs = gpd.overlay(obs_pts, clusterdf, how='intersection')
+
+assignments = pd.read_csv('data_4_assignments/AssignmentsTable_modify.csv')
+
+for cluster_geojson in glob.glob('/Users/rileyhales/code/basin_matching/data_3_pairbasins/geojson-sim-6/*.geojson'):
     # read the geojson of the cluster
-    clusterdf = gpd.read_file(cluster)
-    # identify any of the observation points that are in this cluster (returns a df of points) (should always have 1+)
-    obs = gpd.overlay(obs_pts, clusterdf, how='intersection')
-    # drop any of the basins that have already been assigned an ID to identify which still need assignment
-    clusterdf.drop(clusterdf[clusterdf['COMID'].isin(assigns['AssignedID'].dropna())].index, inplace=True)
-    # identify the stream orders in the basins needing to be assigned
-    stream_orders = sorted(set(assigns[assigns['GeoglowsID'].isin(clusterdf['COMID'])]['Order'].values))
+    clusterdf = gpd.read_file(cluster_geojson)
 
-    print(clusterdf)
+    # Apply option 1: clustering the data by spatially matching the same order gauges inside the clustered basins
+    assignments = assign_gauges_to_spatial_clusters(clusterdf, assignments)
+    assignments.to_csv('data_4_assignments/AssignmentsTable_modify_2.csv', index=False)
 
-    failed_option1 = []
+    # Apply option 2: expanding the search by clustering the gauges
 
-    # todo: apply a scalar from the ratio of the ungauged and gauged upstream areas??
-    # todo: perform a regression of drainage area v cumulative volume??
-
-    # OPTION 1: SpatiallyClustered
-    # There is a gauged stream, of the same order as the ungauged, spatially in your cluster. Preference to the station
-    # with the closest upstream drainage areas??
-    for stream_order in stream_orders:
-        # filter the list of options to be only those who were spatially in the cluster -> those in the obs dataframe
-        options = assigns[assigns['GeoglowsID'].isin(obs['COMID'])]
-        # the filter the remaining options to only include the stream order we're on
-        options = options[options['Order'] == stream_order]
-
-        # if there were no observed points, we cannot apply this option
-        if options.empty:
-            failed_option1.append(stream_order)
-            print(f'For stream order {stream_order}, there were no gauges found')
-            continue
-
-        # if there is only 1 option, apply that one to them all
-        elif len(options) == 1:
-            for i in clusterdf:
-                assigns.loc[assigns['GeoglowsID'] == i, 'AssignedID'] = options['COMID'].values[0]
-                assigns.loc[assigns['GeoglowsID'] == i, 'AssignmentReason'] = 'SpatiallyClustered'
-
-        # if there are multiple options, sort by closest drainage area
-        else:
-            print(clusterdf['COMID'])
-            print(options['GeoglowsID'])
-            # print(clusterdf[clusterdf['COMID'] == options['GeoglowsID'].tolist[0]]['Tot_Drain_'])
-            drainage_areas = [clusterdf[clusterdf['COMID'] == option]['Tot_Drain_'].values[0]
-                              for option in options['GeoglowsID'].tolist()]
-            for i in clusterdf:
-                da = clusterdf[clusterdf['COMID'] == i]['Tot_Drain_'].values[0]
-                station = options[(np.abs(np.array(drainage_areas) - da)).argmin()]
-                print(station)
-                exit()
-
-            pass
-
-        exit()
-        # for option in options:
-        # print(options)
-
-    # OPTION 2: Cluster2Cluster
-    # There is a gauged stream, of the same order as the ungauged, in the cluster a station spatially in the ungauged
-    # cluster. Preference to the station with the closest upstream drainage area and the spatially closest
+    exit()
 
     # OPTION 3: AveragedOrder
 
-    # OPTION 3: Cry and go home
-
-    # of the remaining catchments, figure out their stream orders
-    stream_orders = assigns[assigns['GeoglowsID'].isin(clusterdf['COMID'])]['Order'].values
-    print(stream_orders)
-
+    # OPTION 4: Cry and go home
