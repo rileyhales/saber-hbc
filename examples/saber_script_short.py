c98905814eaf9fef89605cdcc6b2d1b6005280e0
ybaaniya <ybaaniya@byu.edu>
2024-08-15 14:09:53 -0600

    Added gauge cluster in mp_assign_ungauged.

--- examples/saber_script_short.py
+++ examples/saber_script_short.py
@@ -1,10 +1,16 @@
 import logging
-
+import os
 import saber
+import pandas as pd
 
+# END USER INPUTS
 # USER INPUTS - POPULATE THESE PATHS
-config_file = './config.yml'  # Path to the configuration file
-log_path = './log.log'  # leave blank to write to console
+config_file = 'C:\\Users\\ybaaniya\\Downloads\\Yubin\\Bias_correction\\examples\\config.yml'
+log_path = ''
+#the below two line is added by me so that I don't have to run it again
+# Path to the saved assign_df file
+assign_df_file = 'C:\\Users\\ybaaniya\\Downloads\\Yubin\\WORLD BIAS\\WORLD BIAS\\saber workdir\\tables\\assign_table.parquet'# assign_table_bootstrap_file = '/Users/yubinbaaniya/Documents/WORLD BIAS/saber workdir/tables/assign_table_bootstrap.csv'
+# bs_metrics_df_file = '/Users/yubinbaaniya/Documents/WORLD BIAS/saber workdir/tables/bootstrap_metrics.csv'
 # END USER INPUTS
 
 logging.basicConfig(
@@ -17,6 +23,29 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
+
+# #Load already created data
+    if os.path.exists(assign_df_file):
+        logger.info(f'Loading existing assign_df from {assign_df_file}')
+        assign_df = pd.read_parquet(assign_df_file)
+        #print(assign_df)
+    else:
+        raise FileNotFoundError(f"The file {assign_df_file} does not exist.  provide the correct path.")
+#
+#
+#     if os.path.exists(assign_table_bootstrap_file):
+#         logger.info(f'Loading existing bs_assign_df from {assign_table_bootstrap_file}')
+#         bs_assign_df = pd.read_csv(assign_table_bootstrap_file)
+#     else:
+#         logger.info('Performing Bootstrap Validation')
+#         bs_assign_df = saber.bs.mp_table(assign_df)
+#     #
+#     if os.path.exists(bs_metrics_df_file):
+#         logger.info(f'Loading existing assign_df from {bs_metrics_df_file}')
+#         bs_metrics_df = pd.read_csv(bs_metrics_df_file)
+#     else:
+#         raise FileNotFoundError(f"The file {bs_metrics_df_file} does not exist. provide the correct path.")
+
 
     # Read the Config File
     saber.io.read_config(config_file)
@@ -40,13 +69,13 @@ if __name__ == "__main__":
     # bs_assign_df = saber.bs.mp_table(assign_df)
     # bs_metrics_df = saber.bs.mp_metrics(bs_assign_df)
     # saber.bs.postprocess_metrics(bs_metrics_df)
-    saber.bs.histograms()
-    saber.bs.pie_charts()
+    # saber.bs.histograms()
+    # saber.bs.pie_charts()
 
     # Optional - Make all assignments
-    # logger.info('Make Assignments')
-    # assign_df = saber.assign.mp_assign(assign_df)
-    # logger.info('Generating GIS files')
-    # saber.gis.create_maps(assign_df)
+    logger.info('Make Assignments')
+    assign_df = saber.assign.mp_assign(assign_df)
+    logger.info('Generating GIS files')
+    saber.gis.create_maps(assign_df)
 
     logger.info('SABER Completed')
