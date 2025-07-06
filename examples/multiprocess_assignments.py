521037806688d74bdcbf6c299a423a37e4ebb6ca
Riley Hales <39097632+rileyhales@users.noreply.github.com>
2025-07-05 22:21:18 -0600

    Add multithreaded assignment script (#51)

---
+++ examples/multiprocess_assignments.py
@@ -0,0 +1,58 @@
+import logging
+import os
+from multiprocessing import Pool
+
+import pandas as pd
+from tqdm import tqdm
+
+import saber
+from saber.io import COL_ASN_GID, COL_ASN_MID, COL_ASN_REASON, COL_CID, COL_GID, COL_MID
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Global variables for worker processes
+_global_df = None
+_global_cdf = None
+
+
+def init_globals(df, c_df):
+    global _global_df, _global_cdf
+    _global_df = df
+    _global_cdf = c_df
+
+
+def assign_wrapper(mid):
+    return saber.assign._map_assign_ungauged(_global_df, _global_cdf, mid)
+
+
+df = pd.read_parquet('./assign_table.parquet')
+
+if __name__ == '__main__':
+    selector = df[COL_GID].notna()
+    df.loc[selector, COL_ASN_MID] = df[COL_MID]
+    df.loc[selector, COL_ASN_GID] = df[COL_GID]
+    df.loc[selector, COL_ASN_REASON] = 'gauged'
+
+    logger.info('Assign Basins within Clusters')
+
+    for cluster_number in range(df[COL_CID].max() + 1):
+        logger.info(f'Assigning basins in cluster {cluster_number}')
+        c_df = df[df[COL_CID] == cluster_number]
+        mids = c_df[c_df[COL_ASN_REASON] == 'unassigned'][COL_MID].values
+        print(mids.shape)
+
+        with Pool(os.cpu_count(), initializer=init_globals, initargs=(df, c_df)) as p:
+            results = list(tqdm(
+                p.imap(assign_wrapper, mids),
+                total=len(mids),
+                desc=f'Cluster {cluster_number}'
+            ))
+
+        updated = pd.concat(results)
+        df = pd.concat([
+            updated,
+            df[~df[COL_MID].isin(updated[COL_MID])]
+        ]).reset_index(drop=True)
+
+        df.to_parquet(f'./assign_table_after_cluster_{cluster_number}.parquet')
